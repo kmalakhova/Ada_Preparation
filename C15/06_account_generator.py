c793013fb863279efc33d873b2679e3805568883
Ekaterina Malakhova <kgrekhova@gmail.com>
2020-09-08 14:18:46 -0700

    Add an option to read in the student names from a file

--- C15/06_account_generator.py
+++ C15/06_account_generator.py
@@ -1,14 +1,40 @@
 import random 
 
-# fake_students = ['Kate Malakhova', 'Huda Nosikov', 'Niki Ta']
+def get_student_names(count):
+  '''Gets student names from a file or prompt user to enter them manually.'''
+
+  # List that contains student names
+  students = []
+
+  # Ask user if they have a file with student names
+  prompt = f"Please enter filename to upload student names \nor press Enter to type them manually: "
+  filename = input(prompt)
+
+  # If there is a filename
+  if filename != "":
+    with open('student_names.txt') as file_object:
+      for line in file_object:
+        # Check for empty lines
+        if line != "\n":
+          students.append(line.lower()) 
+  
+  # Or get student names from user input
+  else:
+    while len(students) < student_count:
+      prompt = "Enter student first and last name: "
+      student = input(prompt).lower()
+      students.append(student)
+    
+    return students
 
 def generate_id():
-  '''Generate random numbers from 111111 to 999999.'''
+  '''Generates random numbers from 111111 to 999999.'''
   return random.randrange(111111, 999999)
 
 def not_zero_digit(id, count = 3):   
   ''' Determine the last 3 digits of a given number.
   Determine if digit == 0. '''
+
   while count > 0:
     last_digit = id % 10
 
@@ -22,6 +48,7 @@ def not_zero_digit(id, count = 3):
 
 def generate_list_of_ids(count):
   '''Generates the specified count of IDs.'''
+
   # List that stores student ID numbers
   ids = []
   while len(ids) < count:
@@ -36,13 +63,20 @@ def generate_emails(students, ids, domain):
   '''Generates student email addresses.
 
   Email adresses will be generated in a given format: (first initial)+(last name)+(last 3 digits of student ID number)@example.org'''
+
   emails = []
   index = 0
   while index < student_count:
     # Determine first name initial and last name
     full_name = students[index].split(' ')
-    first_name_initial = full_name[0][0]
-    last_name = full_name[1]
+    
+    # Account for first names with a space in them
+    if len(full_name) > 2:
+      first_name_initial = f'{full_name[0][0]}{full_name[1][0]}'
+      last_name = full_name[-1]      
+    else:
+      first_name_initial = full_name[0][0]
+      last_name = full_name[1]
 
     # Determine last 3 didgits od student ID number
     id = str(ids[index])
@@ -60,15 +94,9 @@ def generate_emails(students, ids, domain):
 
 print("This program generates student information.\n")
 
-# Ask the user to enter in 5 student names (first and last names) 
 student_count = 5
-# List that contains student names
-students = []
-while len(students) < student_count:
-  prompt = "Enter student first and last name: "
-  # student = fake_students[len(students)]
-  student = input(prompt).lower()
-  students.append(student)
+
+students = get_student_names(count = student_count)
 
 ids = generate_list_of_ids(count = student_count)
 
